b19335e75a6d42bd3da3206fe44246410cd04c75
NicolasGreskivasd <nicolasgreskiv@hotmail.com>
2024-10-27 00:08:49 -0300

    Update service.js

--- projeto-web/js/service.js
+++ projeto-web/js/service.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8081/api'; // URL base para o backend
+const API_URL = 'http://192.168.191.133:30001/api'; // URL base para o backend
 
 // Função genérica para fazer chamadas GET
 async function getData(endpoint) {
